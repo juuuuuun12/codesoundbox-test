38ae93ed02156d931c65b52a8d13353a930f0724
juuuuuun12 <104136084+juuuuuun12@users.noreply.github.com>
2022-05-03 01:45:09 +0900

    セクション4 - 三項演算子を学習

--- src/index.js
+++ src/index.js
@@ -150,7 +150,7 @@
 /**
  * mapやfilterを使った配列の処理
  */
-const nameArr = ["田中", "山田", "じゅん"];
+// const nameArr = ["田中", "山田", "じゅん"];
 // for (let index = 0; index < nameArr.length; index++) {
 //   console.log(`${index+1}番目は${nameArr[index]}です`);
 // }
@@ -168,11 +168,28 @@ const nameArr = ["田中", "山田", "じゅん"];
 // })
 // console.log(newNumArr);
 
-const newNameArr = nameArr.map((name) => {
-  if (name === "じゅん") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(newNameArr);
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゅん") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 > 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+// const num = 1300;
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています!!" : "許容範囲内です";
+};
+console.log(checkSum(50, 40));
